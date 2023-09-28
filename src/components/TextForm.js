d2b89e49c57862a005a57288e29075f2806ab9d7
nawshar-ali <nowsher.ali28@gmail.com>
2023-09-28 13:36:52 +0600

    add dark mode

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 
 export default function TextForm(props) {
-  const [text, setText] = useState("Enter your text here");
+  const [text, setText] = useState("");
 
   const handleUpClick = () => {
     let newText = text.toUpperCase();
+    setText(newText)
+  }
+
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
     setText(newText)
   }
 
@@ -13,12 +18,22 @@ export default function TextForm(props) {
   }
 
   return (
-    <div>
+    <>
+    <div class="container">
       <div class="mb-3">
         <h1>{props.heading}</h1>
         <textarea class="form-control" onChange={handleOnChange} id="myBox" rows="8" value={text}></textarea>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-primary" onClick={handleLoClick}>Convert to Lowercase</button>
     </div>
+    <div className="container my-3">
+      <h1>Your text summary</h1>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.008 * text.split(" ").length} Minutes read</p>
+      <h2>Preview</h2>
+      <p>{text}</p>
+    </div>
+    </>
   );
 }
